4ee616cfdab72cbd11a6780eec7405cb8f0ab601
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-02-21 03:47:57 +0000

    add vector db utils

---
+++ engine/vector_db.py
@@ -0,0 +1,70 @@
+import chromadb
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core.vector_stores import VectorStoreQuery
+
+from engine import config
+
+__db = None
+
+
+def get_db(path=config.chroma_db_dir):
+    global __db
+    if __db is None:
+        __db = chromadb.PersistentClient(path)
+    return __db
+
+
+def get_collection(collection_name):
+    db = get_db()
+    return db.get_or_create_collection(collection_name)
+
+
+def get_vector_store(data_name):
+    collection_name = __get_collection_name(data_name)
+    chroma_collection = get_collection(collection_name)
+    return ChromaVectorStore(chroma_collection=chroma_collection)
+
+
+def has_data(vector_store):
+    ids = vector_store.query(VectorStoreQuery()).ids
+    return len(ids) != 0
+
+
+def __delete_collection(collection_name):
+    db = get_db()
+    db.delete_collection(collection_name)
+
+
+def __get_collection_name(data_name):
+    return "c_" + data_name
+
+
+def __show_db():
+    collections = get_db().list_collections()
+    print("collections size:", len(collections))
+    print("===================")
+    for collection in collections:
+        print(collection)
+        count = collection.count()
+        print("record count:", count)
+        vectors = collection.peek(1)
+        for embeddings in vectors["embeddings"]:
+            print("embeddings dimension:", len(embeddings))
+            print(embeddings[:4])
+        print("-------------------")
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) > 2:
+        if sys.argv[1] == "rm":
+            collection = len(sys.argv) == 3 and sys.argv[2] or None
+            if collection is None:
+                print("provide the collection name")
+            else:
+                __delete_collection(collection)
+        else:
+            print("rm is only one supported cmd")
+    else:
+        __show_db()
