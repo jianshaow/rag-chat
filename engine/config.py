183aaea968a2f4f1a68f4191a6820c48011eb6ea
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-02-21 03:46:59 +0000

    add multi embed model support

--- engine/config.py
+++ engine/config.py
@@ -1,6 +1,9 @@
 import os
-from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.embeddings.openai import OpenAIEmbedding, OpenAIEmbeddingModelType
 from llama_index.llms.openai import OpenAI
+
+ZH_EMBED_MODEL = "bge-large-zh-v1.5"
+EN_EMBED_MODEL = "bge-large-en-v1.5"
 
 data_root_dir = os.environ.get("DATA_ROOT_DIR", "data")
 chroma_db_dir = os.environ.get("CHROMA_DB_DIR", "chroma_db")
@@ -8,8 +11,14 @@ default_data_name = "__root"
 default_question = "What did the author do growing up?"
 
 
-def embedding_model():
-    return OpenAIEmbedding()
+def embed_model(data_name=default_data_name):
+    if data_name.startswith("en"):
+        model = EN_EMBED_MODEL
+    elif data_name.startswith("zh"):
+        model = ZH_EMBED_MODEL
+    else:
+        model = OpenAIEmbeddingModelType.TEXT_EMBED_ADA_002
+    return OpenAIEmbedding(model=model)
 
 
 def chat_model():
--- engine/indexer.py
+++ engine/indexer.py
@@ -1,40 +1,33 @@
-import chromadb
 from llama_index.core import (
-    Settings,
     StorageContext,
     VectorStoreIndex,
     SimpleDirectoryReader,
 )
-from llama_index.vector_stores.chroma import ChromaVectorStore
 
-from engine import config, data_store
+from engine import config, vector_db, data_store
 
 __indexes = {}
 
 
-def initialize_index(data_name, data_path):
-    global __indexes
-    db = chromadb.PersistentClient(path=config.chroma_db_dir)
-    collection_name = __get_collection_name(data_name)
-    chroma_collection = db.get_or_create_collection(collection_name)
-    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-    Settings.embed_model = config.embedding_model()
-    Settings.llm = config.chat_model()
-    print("embed_model:", Settings.embed_model.model_name)
-    print("chat_model:", Settings.llm.model)
-    if chroma_collection.count() == 0:
+def create_or_load_index(data_name, data_path):
+    vector_store = vector_db.get_vector_store(data_name)
+    embed_model = config.embed_model()
+    print("data_name:", data_name)
+    print("embed_model:", embed_model.model_name)
+    if vector_db.has_data(vector_store):
+        index = VectorStoreIndex.from_vector_store(
+            vector_store,
+            embed_model,
+        )
+    else:
         documents = SimpleDirectoryReader(data_path).load_data(show_progress=True)
         storage_context = StorageContext.from_defaults(vector_store=vector_store)
         index = VectorStoreIndex.from_documents(
             documents,
-            storage_context=storage_context,
+            storage_context,
+            embed_model=embed_model,
             show_progress=True,
         )
-    else:
-        index = VectorStoreIndex.from_vector_store(
-            vector_store,
-        )
-    __indexes[data_name] = index
     return index
 
 
@@ -44,12 +37,9 @@ def get_index(data_name=config.default_data_name):
     if index is None:
         data_path = data_store.get_data_path(data_name)
         if data_path is not None:
-            index = initialize_index(data_name, data_path)
+            index = create_or_load_index(data_name, data_path)
+    __indexes[data_name] = index
     return index
-
-
-def __get_collection_name(data_name):
-    return "c_" + data_name
 
 
 if __name__ == "__main__":
