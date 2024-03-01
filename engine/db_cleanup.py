26c76ece94621c219af7ed6a76da1727bdaa5796
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-03-01 14:22:28 +0800

    add db utils

---
+++ engine/db_cleanup.py
@@ -0,0 +1,8 @@
+import sys, chromadb
+
+db = chromadb.PersistentClient(path="chroma_db")
+collection = len(sys.argv) == 2 and sys.argv[1] or None
+if collection is None:
+    print("provide the collection name")
+else:
+    db.delete_collection(collection)
