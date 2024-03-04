ee2296b85ca58300baab77ef482414eb8cd97249
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-03-04 03:19:33 +0000

    add env support

--- engine/config.py
+++ engine/config.py
@@ -2,22 +2,28 @@ import os
 from llama_index.embeddings.openai import OpenAIEmbedding, OpenAIEmbeddingModelType
 from llama_index.llms.openai import OpenAI
 
-EN_EMBED_MODEL = OpenAIEmbeddingModelType.TEXT_EMBED_ADA_002
-ZH_EMBED_MODEL = OpenAIEmbeddingModelType.TEXT_EMBED_3_LARGE
-
 data_root_dir = os.environ.get("DATA_ROOT_DIR", "data")
 chroma_db_dir = os.environ.get("CHROMA_DB_DIR", "chroma_db")
+
+en_embed_model = os.environ.get(
+    "EN_EMBED_MODEL", OpenAIEmbeddingModelType.TEXT_EMBED_ADA_002
+)
+zh_embed_model = os.environ.get(
+    "ZH_EMBED_MODEL", OpenAIEmbeddingModelType.TEXT_EMBED_3_LARGE
+)
+default_embed_model = OpenAIEmbeddingModelType.TEXT_EMBED_ADA_002
+
 default_data_name = "__root"
 default_question = "What did the author do growing up?"
 
 
 def embed_model(data_name=default_data_name):
     if data_name.startswith("en"):
-        model = EN_EMBED_MODEL
+        model = en_embed_model
     elif data_name.startswith("zh"):
-        model = ZH_EMBED_MODEL
+        model = zh_embed_model
     else:
-        model = OpenAIEmbeddingModelType.TEXT_EMBED_ADA_002
+        model = default_embed_model
     return OpenAIEmbedding(model=model)
 
 
