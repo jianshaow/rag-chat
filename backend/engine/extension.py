369870f5160c56538f38e9b46dfc7ed6ec400fe8
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-07-17 07:44:57 +0000

    add get models for extension

--- backend/engine/extension.py
+++ backend/engine/extension.py
@@ -10,6 +10,10 @@ def update_api_config(api_spec: str, conf: dict):
     raise NotImplementedError(f"API spec '{api_spec}' is not implemented")
 
 
+def get_models(api_spec: str) -> list:
+    raise NotImplementedError(f"API spec '{api_spec}' is not implemented")
+
+
 def new_model(api_spec, model_type):
     raise NotImplementedError(
         f"Model type '{model_type}' on API spec '{api_spec}' is not implemented"
