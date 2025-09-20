a79c85425e214da8566705b234a48aa96a5ef48d
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-09-20 02:52:11 +0000

    fix issue

--- backend/app/engine/data_store.py
+++ backend/app/engine/data_store.py
@@ -1,7 +1,8 @@
 import os
+from typing import Dict
 
 import yaml
-from pydantic import BaseModel
+from pydantic import BaseModel, RootModel
 
 from app.engine import UPLOADED_DATA_DIR, setting
 
@@ -10,15 +11,32 @@ class DataConfig(BaseModel):
     default_question: str
 
 
+class DataConfigs(RootModel[Dict[str, DataConfig]]):
+
+    def __getitem__(self, key: str) -> DataConfig:
+        return self.root[key]
+
+    def keys(self):
+        return self.root.keys()
+
+    def items(self):
+        return self.root.items()
+
+    def values(self):
+        return self.root.values()
+
+
 DEFAULT_DATA_CONFIG = DataConfig(default_question="What is the document about?")
 
 
-__builtin_data_config: dict[str, DataConfig] = {
-    "en_novel": DataConfig(default_question="What did the author do growing up?"),
-    "zh_novel": DataConfig(default_question="地球发动机都安装在哪里？"),
-}
+__builtin_data_config = DataConfigs.model_validate(
+    {
+        "en_novel": DataConfig(default_question="What did the author do growing up?"),
+        "zh_novel": DataConfig(default_question="地球发动机都安装在哪里？"),
+    }
+)
 
-__data_config: dict[str, DataConfig] = __builtin_data_config
+__data_configs: DataConfigs = __builtin_data_config
 
 data_config_file = os.environ.get(
     "DATA_CONFIG", os.path.join(setting.get_data_base_dir(), "data_config.yaml")
@@ -26,7 +44,7 @@ data_config_file = os.environ.get(
 
 if data_config_file and os.path.isfile(data_config_file):
     with open(data_config_file, "r", encoding="utf-8") as file:
-        __data_config.update(yaml.safe_load(file))
+        __data_configs.root.update(yaml.safe_load(file))
 
 
 def get_data_config():
@@ -39,8 +57,8 @@ def get_data_config():
                 os.path.join(data_base_dir, data_dir)
             ):
                 data_config[data_dir] = (
-                    __data_config[data_dir]
-                    if data_dir in __data_config
+                    __data_configs[data_dir]
+                    if data_dir in __data_configs.keys()
                     else DEFAULT_DATA_CONFIG
                 )
     return data_config
