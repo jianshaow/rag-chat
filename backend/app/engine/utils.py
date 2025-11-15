b7d5a90d7f3848fa2b04f96be8ab42de7856efd8
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-11-15 06:03:07 +0000

    improve printing

--- backend/app/engine/utils.py
+++ backend/app/engine/utils.py
@@ -6,20 +6,40 @@ logger = logging.getLogger(__name__)
 MODEL_INFO_LOG_TEMPLATE = """
 %s
 moel_provider: %s
-data_name: %s
 embed_model: %s
 chat_model: %s
 %s
 """
 
+TOOL_INFO_LOG_TEMPLATE = """
+%s
+tool_name: %s
+tool_config: %s
+%s
+"""
 
-def log_model_info(data_dir: str):
+
+def log_model_info():
     logger.info(
         MODEL_INFO_LOG_TEMPLATE,
         "-" * 80,
         setting.get_model_provider(),
-        data_dir,
         models.get_embed_model_name(),
         models.get_chat_model_name(),
+        "-" * 80,
+    )
+
+
+def log_tool_info(data_dir: str):
+    tool_set = setting.get_tool_set()
+    if tool_set == "retriever":
+        tool_config = f"data_dir={data_dir}"
+    else:
+        tool_config = f"mcp_server={setting.get_mcp_server()}"
+    logger.info(
+        TOOL_INFO_LOG_TEMPLATE,
+        "-" * 80,
+        setting.get_tool_set(),
+        tool_config,
         "-" * 80,
     )
