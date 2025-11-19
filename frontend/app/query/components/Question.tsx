12c5148659f3ba1433afa5129254ff5d4df2aabc
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-11-19 17:42:29 +0000

    add qustion component

---
+++ frontend/app/query/components/Question.tsx
@@ -0,0 +1,31 @@
+// import React, { useEffect, useRef } from "react";
+// import './Question.css';
+
+// interface QuestionProps {
+//   events: string[];
+//   height?: number | string;
+// }
+
+// const Question: React.FC<QuestionProps> = ({ events, height = "3em" }) => {
+//   const eventsRef = useRef<HTMLDivElement>(null);
+
+
+
+//   return (
+//     <div className='question-block'>
+//       <label>Question</label>
+//       <form onSubmit={this.handleQuestion}>
+//         <div className='question-bar'>
+//           <input type='text' value={request}
+//             onChange={(e: ChangeEvent<HTMLInputElement>) => {
+//               this.setState({ request: e.target.value });
+//             }}
+//             style={{ width: '100%' }} />
+//           <button type='submit'>Submit</button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default Question
